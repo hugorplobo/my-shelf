66b62148aaa31d4ef92515da49f490c42f57ae62
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-12-03 21:34:14 -0300

    chore: setup project

---
+++ src/main/java/com/hugorplobo/myshelf/MyshelfApplication.java
@@ -0,0 +1,13 @@
+package com.hugorplobo.myshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugorplobo/myshelf/MyshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugorplobo.myshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
